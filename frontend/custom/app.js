de1bb4347ed7d44a0de16924eb129eee283241f1
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-10-18 15:24:48 -0400

    Modify FB Pixel code

--- frontend/custom/app.js
+++ frontend/custom/app.js
@@ -2,19 +2,6 @@ window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createE 1lement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window,document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
- fbq('init', '1825684530987316');
- fbq('track', 'PageView');
-</script>
-
 function urlencodeFormData(fd){
     var s = '';
     function encode(s){ return encodeURIComponent(s).replace(/%20/g,'+'); }
